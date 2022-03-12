9c2ecb8fd7c91b7a10f4b33956b277a29565ebe5
Shikhar <ishgupta2015@gmail.com>
2022-03-12 10:47:18 +0530

    Load config from env or file

---
+++ backend/cmd/serve.go
@@ -0,0 +1,35 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// serveCmd represents the serve command
+var serveCmd = &cobra.Command{
+	Use:   "serve",
+	Short: "Starts http server",
+	Long:  `Starts the http server with configured options`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("serve called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(serveCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// serveCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// serveCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
