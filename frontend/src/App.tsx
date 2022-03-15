252d599e46f668ba980feb00fe2b0c29c91027d4
Shikhar <ishgupta2015@gmail.com>
2022-03-15 16:42:33 +0530

    Add mui component library

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
