40bba5b9cd2209b7254bfc50ca7f92297f2017c3
vivekweb2013 <vivekweb2013@gmail.com>
2022-03-21 09:22:51 +0530

    Hide drawer on small screens

--- frontend/src/components/AppBar.tsx
+++ frontend/src/components/AppBar.tsx
@@ -1,37 +1,17 @@
-import { Login as LoginIcon, Menu as MenuIcon } from '@mui/icons-material';
-import { Avatar, Box, Button, CircularProgress, IconButton, Menu, MenuItem, styled, Toolbar } from '@mui/material';
-import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
+import { Login as LoginIcon } from '@mui/icons-material';
+import { Avatar, Box, Button, CircularProgress, Menu, MenuItem, Toolbar, Typography } from '@mui/material';
+import AppBarComponent from '@mui/material/AppBar';
 import React from 'react';
 import { User, UserStatus } from '../reducer/user/userSlice';
-import { DRAWER_WIDTH } from './AppDrawer';
-
-
-interface AppBarProps extends MuiAppBarProps {
-  open?: boolean;
-}
 
 interface Props {
   user: User | null
   userStatus: UserStatus
   setUserStatus: (userStatus: UserStatus) => void
   handleLogout: () => void
-  isOpen: boolean
-  toggleDrawer: (isOpen: boolean) => void
 }
 
-const AppBarComponent = styled(MuiAppBar, { shouldForwardProp: (prop) => prop !== 'open' })<AppBarProps>(({ theme, open }) => ({
-  zIndex: theme.zIndex.drawer + 1, transition: theme.transitions.create(['width', 'margin'], {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }), ...(open && {
-    marginLeft: DRAWER_WIDTH, width: `calc(100% - ${DRAWER_WIDTH}px)`,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp, duration: theme.transitions.duration.enteringScreen,
-    }),
-  }),
-}));
-
-const AppBar: React.FC<Props> = ({ user, userStatus, setUserStatus, handleLogout, isOpen, toggleDrawer }) => {
+const AppBar: React.FC<Props> = ({ user, userStatus, setUserStatus, handleLogout }) => {
   const isLoading = userStatus === UserStatus.LOADING
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
 
@@ -43,29 +23,29 @@ const AppBar: React.FC<Props> = ({ user, userStatus, setUserStatus, handleLogout
     setAnchorEl(null);
   };
 
-  return (<AppBarComponent position="absolute" open={isOpen}>
-    <Toolbar sx={{ pr: '24px', /* keep right padding when drawer closed */ }}>
-      <IconButton edge="start" color="inherit" aria-label="open drawer" onClick={() => toggleDrawer(isOpen)}
-        sx={{ marginRight: '36px', ...(isOpen && { display: 'none' }), }}>
-        <MenuIcon />
-      </IconButton>
-      <Box sx={{ flexGrow: 1 }}></Box> {user == null ?
-        (!isLoading ? <Button color="inherit" href="/api/v1/oauth2/login/github" endIcon={<LoginIcon />}
-          onClick={() => setUserStatus(UserStatus.LOADING)}>Login</Button> :
-          <CircularProgress color="inherit" />)
-        :
-        <>
-          <Avatar onClick={handleMenu} alt={user.name} src={user.avatar_url} sx={{ "cursor": "pointer" }}></Avatar>
-          <Menu autoFocus={false} sx={{ mt: '45px' }} id="menu-appbar" anchorEl={anchorEl} anchorOrigin={{
-            vertical: 'top', horizontal: 'right'
-          }} transformOrigin={{ vertical: 'top', horizontal: 'right', }} open={Boolean(anchorEl)} onClose={handleClose}>
-            <MenuItem onClick={handleClose}>Profile</MenuItem>
-            <MenuItem onClick={handleLogout}>Logout</MenuItem>
-          </Menu>
-        </>
-      }
-    </Toolbar>
-  </AppBarComponent>
+  return (
+    <AppBarComponent position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
+      <Toolbar>
+      <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1, display: "flex" }}>
+          GIT NOTER
+        </Typography>
+        <Box sx={{ flexGrow: 1 }}></Box> {user == null ?
+          (!isLoading ? <Button color="inherit" href="/api/v1/oauth2/login/github" endIcon={<LoginIcon />}
+            onClick={() => setUserStatus(UserStatus.LOADING)}>Login</Button> :
+            <CircularProgress color="inherit" />)
+          :
+          <>
+            <Avatar onClick={handleMenu} alt={user.name} src={user.avatar_url} sx={{ "cursor": "pointer" }}></Avatar>
+            <Menu autoFocus={false} sx={{ mt: '5px' }} id="menu-appbar" anchorEl={anchorEl} anchorOrigin={{
+              vertical: 'bottom', horizontal: 'right'
+            }} transformOrigin={{ vertical: 'top', horizontal: 'right', }} open={Boolean(anchorEl)} onClose={handleClose}>
+              <MenuItem onClick={handleClose}>Profile</MenuItem>
+              <MenuItem onClick={handleLogout}>Logout</MenuItem>
+            </Menu>
+          </>
+        }
+      </Toolbar>
+    </AppBarComponent>
   )
 }
 
