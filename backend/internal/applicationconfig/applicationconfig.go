batnoter/batnoter-api

Store & retrieve notes with github instead of db

internal/applicationconfig/applicationconfig.go
@@ -3,22 +3,34 @@ package applicationconfig
 import (
 	"github.com/vivekweb2013/batnoter/internal/auth"
 	"github.com/vivekweb2013/batnoter/internal/config"
-	"github.com/vivekweb2013/batnoter/internal/note"
+	"github.com/vivekweb2013/batnoter/internal/github"
 	"github.com/vivekweb2013/batnoter/internal/preference"
 	"github.com/vivekweb2013/batnoter/internal/user"
+	"golang.org/x/oauth2"
+	gh "golang.org/x/oauth2/github"
 	"gorm.io/gorm"
 )
 
 type ApplicationConfig struct {
 	Config            config.Config
 	DB                *gorm.DB
+	OAuth2Config      oauth2.Config
 	AuthService       auth.Service
 	UserService       user.Service
 	PreferenceService preference.Service
-	NoteService       note.Service
+	GithubService     github.Service
 }
 
 func NewApplicationConfig(config config.Config, db *gorm.DB) *ApplicationConfig {
+	// create github oauth2 config
+	oauth2Config := oauth2.Config{
+		RedirectURL:  config.OAuth2.Github.RedirectURL,
+		ClientID:     config.OAuth2.Github.ClientID,
+		ClientSecret: config.OAuth2.Github.ClientSecret,
+		Scopes:       []string{"read:user", "user:email", "repo"},
+		Endpoint:     gh.Endpoint,
+	}
+
 	authService := auth.NewService(auth.TokenConfig{
 		SecretKey: config.App.SecretKey,
 		Issuer:    "https://batnoter.com",
@@ -28,15 +40,16 @@ func NewApplicationConfig(config config.Config, db *gorm.DB) *ApplicationConfig 
 	preferenceRepo := preference.NewRepository(db)
 	preferenceService := preference.NewService(preferenceRepo)
 
-	noteRepo := note.NewRepository(db)
-	noteService := note.NewService(noteRepo)
+	githubClientBuilder := github.NewClientBuilder(&oauth2Config)
+	githubService := github.NewService(githubClientBuilder)
 
 	return &ApplicationConfig{
 		Config:            config,
 		DB:                db,
+		OAuth2Config:      oauth2Config,
 		AuthService:       authService,
 		UserService:       userService,
 		PreferenceService: preferenceService,
-		NoteService:       noteService,
+		GithubService:     githubService,
 	}
 }

internal/httpservice/login.go
@@ -1,0 +1,124 @@
+package httpservice
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	gh "github.com/google/go-github/v43/github"
+	"github.com/google/uuid"
+	"github.com/sirupsen/logrus"
+	"github.com/vivekweb2013/batnoter/internal/auth"
+	"github.com/vivekweb2013/batnoter/internal/github"
+	"github.com/vivekweb2013/batnoter/internal/user"
+)
+
+type UserResponsePayload struct {
+	Email       string       `json:"email"`
+	Name        string       `json:"name,omitempty"`
+	Location    string       `json:"location,omitempty"`
+	AvatarURL   string       `json:"avatar_url,omitempty"`
+	DisabledAt  *time.Time   `json:"disabled_at,omitempty"`
+	DefaultRepo *RepoPayload `json:"default_repo,omitempty"`
+}
+
+type LoginHandler struct {
+	authService  auth.Service
+	githubServie github.Service
+	userService  user.Service
+}
+
+func NewLoginHandler(authService auth.Service, githubServie github.Service, userService user.Service) *LoginHandler {
+	return &LoginHandler{
+		authService:  authService,
+		githubServie: githubServie,
+		userService:  userService,
+	}
+}
+
+func (l *LoginHandler) GithubLogin(c *gin.Context) {
+	state := uuid.NewString()
+	c.SetCookie("state", state, 600, "/", "", true, true)
+
+	url := l.githubServie.GetAuthCodeURL(state)
+
+	// trigger authorization code grant flow
+	c.Redirect(http.StatusTemporaryRedirect, url)
+}
+
+func (l *LoginHandler) GithubOAuth2Callback(c *gin.Context) {
+	logrus.Info("github oauth2 callback started")
+	state, _ := c.Cookie("state")
+	stateFromCallback := c.Query("state")
+	code := c.Query("code")
+	failRedirectPath := "/?login_error=true"
+
+	if stateFromCallback != state {
+		logrus.Error("invalid oauth state")
+		c.Redirect(http.StatusTemporaryRedirect, failRedirectPath)
+		return
+	}
+
+	githubToken, err := l.githubServie.GetToken(c, code)
+	if err != nil {
+		logrus.Errorf("auth code exchange for token failed: %s", err.Error())
+		c.Redirect(http.StatusTemporaryRedirect, failRedirectPath)
+		return
+	}
+
+	githubUser, err := l.githubServie.GetUser(c, githubToken)
+	if err != nil {
+		logrus.Errorf("retrieving user from github failed: %s", err.Error())
+		c.Redirect(http.StatusTemporaryRedirect, failRedirectPath)
+		return
+	}
+
+	// get user from db if exists
+	dbUser, err := l.userService.GetByEmail(*githubUser.Email)
+	if err != nil {
+		logrus.Errorf("retrieving user from db using email failed: %s", err.Error())
+		c.Redirect(http.StatusTemporaryRedirect, failRedirectPath)
+		return
+	}
+	githubTokenJSON, err := json.Marshal(githubToken)
+	if err != nil {
+		logrus.Errorf("converting github token to json failed: %s", err.Error())
+		c.Redirect(http.StatusTemporaryRedirect, failRedirectPath)
+		return
+	}
+	mapUserAttributes(&dbUser, string(githubTokenJSON), githubUser)
+
+	// create/update the user record
+	userID, err := l.userService.Save(dbUser)
+	if err != nil {
+		logrus.Errorf("saving user to db failed: %s", err.Error())
+		c.Redirect(http.StatusTemporaryRedirect, failRedirectPath)
+		return
+	}
+
+	appToken, err := l.authService.GenerateToken(userID)
+	if err != nil {
+		logrus.Errorf("token generation failed: %s", err.Error())
+		c.Redirect(http.StatusTemporaryRedirect, failRedirectPath)
+		return
+	}
+
+	// for security reasons, avoid using cookies to send the token to client
+	// instead use html with a script that stores the token to localstorage and redirects to homepage
+	// this is the only workaround to send token to client without using cookies
+	// since the client(frontend) can only read headers/response with ajax request, and this call is not ajax
+	c.Header("Content-Type", "text/html")
+	c.String(200, `<!DOCTYPE html><html><body><script>(function(){localStorage.setItem("token","%s");location.replace("/");}());</script></body></html>`, appToken)
+	logrus.Info("github oauth2 callback finished")
+}
+
+func mapUserAttributes(dbUser *user.User, ghToken string, githubUser gh.User) {
+	dbUser.GithubToken = ghToken
+	dbUser.Email = githubUser.GetEmail()
+	dbUser.Name = githubUser.GetName()
+	dbUser.Location = githubUser.GetLocation()
+	dbUser.AvatarURL = githubUser.GetAvatarURL()
+	dbUser.GithubID = githubUser.GetID()
+	dbUser.GithubUsername = githubUser.GetLogin()
+}

internal/httpservice/login_test.go
@@ -1,0 +1,140 @@
+package httpservice
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang/mock/gomock"
+	gh "github.com/google/go-github/v43/github"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+	"github.com/vivekweb2013/batnoter/internal/auth"
+	"github.com/vivekweb2013/batnoter/internal/github"
+	"github.com/vivekweb2013/batnoter/internal/preference"
+	"github.com/vivekweb2013/batnoter/internal/user"
+	"golang.org/x/oauth2"
+	"gorm.io/gorm"
+)
+
+const (
+	userID          = uint(1012)
+	email           = "john.doe@example.com"
+	name            = "John Doe"
+	location        = "New York"
+	avatarURL       = "http://example.com/avatar"
+	oauth2TokenJSON = `{"access_token":"gho_token","token_type":"bearer","expiry":"0001-01-01T00:00:00Z"}`
+)
+
+const ()
+
+func TestGithubLogin(t *testing.T) {
+	t.Run("should redirect to provider when the github login request is valid", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+		githubService := github.NewMockService(ctrl)
+
+		gin.SetMode(gin.TestMode)
+		router := gin.Default()
+		handler := NewLoginHandler(nil, githubService, nil)
+		githubService.EXPECT().GetAuthCodeURL(gomock.Any()).Return("/")
+
+		router.GET("/api/v1/oauth2/login/github", handler.GithubLogin)
+		response := httptest.NewRecorder()
+		req, _ := http.NewRequest(http.MethodGet, "/api/v1/oauth2/login/github", nil)
+
+		router.ServeHTTP(response, req)
+		assert.Equal(t, http.StatusTemporaryRedirect, response.Code)
+	})
+}
+
+func TestGithubOAuth2Callback(t *testing.T) {
+	t.Run("should save the user(with token) & return token response when callback invoked", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+		authService := auth.NewMockService(ctrl)
+		githubService := github.NewMockService(ctrl)
+		userService := user.NewMockService(ctrl)
+		state := uuid.NewString()
+		authCode := "abcd"
+		appToken := "app_token"
+		var oauthToken oauth2.Token
+		json.Unmarshal([]byte(oauth2TokenJSON), &oauthToken)
+		githubUser := validGithubUser()
+		dbUser := makeDBUser(githubUser, oauth2TokenJSON)
+
+		gin.SetMode(gin.TestMode)
+		router := gin.Default()
+		handler := NewLoginHandler(authService, githubService, userService)
+		githubService.EXPECT().GetToken(gomock.Any(), authCode).Return(oauthToken, nil)
+		githubService.EXPECT().GetUser(gomock.Any(), oauthToken).Return(githubUser, nil)
+		userService.EXPECT().GetByEmail(email).Return(dbUser, nil)
+		userService.EXPECT().Save(dbUser).Return(uint(1), nil)
+		authService.EXPECT().GenerateToken(uint(1)).Return(appToken, nil)
+
+		router.GET("/oauth2/github/callback", handler.GithubOAuth2Callback)
+		response := httptest.NewRecorder()
+		cookie := http.Cookie{
+			Name:     "state",
+			Value:    state,
+			Path:     "/",
+			Expires:  time.Now().Add(10 * time.Minute),
+			HttpOnly: true,
+		}
+		req, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("/oauth2/github/callback?code=%s&state=%s", authCode, state), nil)
+		req.AddCookie(&cookie)
+
+		router.ServeHTTP(response, req)
+		assert.Equal(t, http.StatusOK, response.Code)
+		assert.Contains(t, response.Body.String(), appToken)
+	})
+}
+
+func makeDBUser(githubUser gh.User, token string) user.User {
+	return user.User{
+		Model: gorm.Model{
+			ID:        1,
+			CreatedAt: time.Now(),
+			UpdatedAt: time.Now(),
+		},
+		Email:          *githubUser.Email,
+		Name:           *githubUser.Name,
+		Location:       *githubUser.Location,
+		AvatarURL:      *githubUser.AvatarURL,
+		GithubID:       *githubUser.ID,
+		GithubUsername: *githubUser.Login,
+		GithubToken:    token,
+		DefaultRepo: &preference.DefaultRepo{
+			Model: gorm.Model{
+				ID:        1,
+				CreatedAt: time.Time{},
+				UpdatedAt: time.Time{},
+			},
+			UserID:        1,
+			Name:          repository,
+			Visibility:    visibility,
+			DefaultBranch: branch,
+		},
+	}
+}
+
+func validGithubUser() gh.User {
+	testEmail := email
+	testName := name
+	testLocation := location
+	testAvatarURL := avatarURL
+	githubID := int64(12345)
+	githubUsername := "johndoe"
+	return gh.User{
+		ID:        &githubID,
+		Login:     &githubUsername,
+		Email:     &testEmail,
+		Name:      &testName,
+		Location:  &testLocation,
+		AvatarURL: &testAvatarURL,
+	}
+}

batnoter/batnoter

backend/internal/applicationconfig/applicationconfig.go
@@ -3,22 +3,34 @@ package applicationconfig
 import (
 	"github.com/vivekweb2013/gitnoter/internal/auth"
 	"github.com/vivekweb2013/gitnoter/internal/config"
-	"github.com/vivekweb2013/gitnoter/internal/note"
+	"github.com/vivekweb2013/gitnoter/internal/github"
 	"github.com/vivekweb2013/gitnoter/internal/preference"
 	"github.com/vivekweb2013/gitnoter/internal/user"
+	"golang.org/x/oauth2"
+	gh "golang.org/x/oauth2/github"
 	"gorm.io/gorm"
 )
 
 type ApplicationConfig struct {
 	Config            config.Config
 	DB                *gorm.DB
+	OAuth2Config      oauth2.Config
 	AuthService       auth.Service
 	UserService       user.Service
 	PreferenceService preference.Service
-	NoteService       note.Service
+	GithubService     github.Service
 }
 
 func NewApplicationConfig(config config.Config, db *gorm.DB) *ApplicationConfig {
+	// create github oauth2 config
+	oauth2Config := oauth2.Config{
+		RedirectURL:  config.OAuth2.Github.RedirectURL,
+		ClientID:     config.OAuth2.Github.ClientID,
+		ClientSecret: config.OAuth2.Github.ClientSecret,
+		Scopes:       []string{"read:user", "user:email", "repo"},
+		Endpoint:     gh.Endpoint,
+	}
+
 	authService := auth.NewService(auth.TokenConfig{
 		SecretKey: config.App.SecretKey,
 		Issuer:    "https://gitnoter.com",
@@ -28,15 +40,16 @@ func NewApplicationConfig(config config.Config, db *gorm.DB) *ApplicationConfig 
 	preferenceRepo := preference.NewRepository(db)
 	preferenceService := preference.NewService(preferenceRepo)
 
-	noteRepo := note.NewRepository(db)
-	noteService := note.NewService(noteRepo)
+	githubClientBuilder := github.NewClientBuilder(&oauth2Config)
+	githubService := github.NewService(githubClientBuilder)
 
 	return &ApplicationConfig{
 		Config:            config,
 		DB:                db,
+		OAuth2Config:      oauth2Config,
 		AuthService:       authService,
 		UserService:       userService,
 		PreferenceService: preferenceService,
-		NoteService:       noteService,
+		GithubService:     githubService,
 	}
 }

backend/internal/httpservice/login.go
@@ -1,0 +1,124 @@
+package httpservice
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	gh "github.com/google/go-github/v43/github"
+	"github.com/google/uuid"
+	"github.com/sirupsen/logrus"
+	"github.com/vivekweb2013/gitnoter/internal/auth"
+	"github.com/vivekweb2013/gitnoter/internal/github"
+	"github.com/vivekweb2013/gitnoter/internal/user"
+)
+
+type UserResponsePayload struct {
+	Email       string       `json:"email"`
+	Name        string       `json:"name,omitempty"`
+	Location    string       `json:"location,omitempty"`
+	AvatarURL   string       `json:"avatar_url,omitempty"`
+	DisabledAt  *time.Time   `json:"disabled_at,omitempty"`
+	DefaultRepo *RepoPayload `json:"default_repo,omitempty"`
+}
+
+type LoginHandler struct {
+	authService  auth.Service
+	githubServie github.Service
+	userService  user.Service
+}
+
+func NewLoginHandler(authService auth.Service, githubServie github.Service, userService user.Service) *LoginHandler {
+	return &LoginHandler{
+		authService:  authService,
+		githubServie: githubServie,
+		userService:  userService,
+	}
+}
+
+func (l *LoginHandler) GithubLogin(c *gin.Context) {
+	state := uuid.NewString()
+	c.SetCookie("state", state, 600, "/", "", true, true)
+
+	url := l.githubServie.GetAuthCodeURL(state)
+
+	// trigger authorization code grant flow
+	c.Redirect(http.StatusTemporaryRedirect, url)
+}
+
+func (l *LoginHandler) GithubOAuth2Callback(c *gin.Context) {
+	logrus.Info("github oauth2 callback started")
+	state, _ := c.Cookie("state")
+	stateFromCallback := c.Query("state")
+	code := c.Query("code")
+	failRedirectPath := "/?login_error=true"
+
+	if stateFromCallback != state {
+		logrus.Error("invalid oauth state")
+		c.Redirect(http.StatusTemporaryRedirect, failRedirectPath)
+		return
+	}
+
+	githubToken, err := l.githubServie.GetToken(c, code)
+	if err != nil {
+		logrus.Errorf("auth code exchange for token failed: %s", err.Error())
+		c.Redirect(http.StatusTemporaryRedirect, failRedirectPath)
+		return
+	}
+
+	githubUser, err := l.githubServie.GetUser(c, githubToken)
+	if err != nil {
+		logrus.Errorf("retrieving user from github failed: %s", err.Error())
+		c.Redirect(http.StatusTemporaryRedirect, failRedirectPath)
+		return
+	}
+
+	// get user from db if exists
+	dbUser, err := l.userService.GetByEmail(*githubUser.Email)
+	if err != nil {
+		logrus.Errorf("retrieving user from db using email failed: %s", err.Error())
+		c.Redirect(http.StatusTemporaryRedirect, failRedirectPath)
+		return
+	}
+	githubTokenJSON, err := json.Marshal(githubToken)
+	if err != nil {
+		logrus.Errorf("converting github token to json failed: %s", err.Error())
+		c.Redirect(http.StatusTemporaryRedirect, failRedirectPath)
+		return
+	}
+	mapUserAttributes(&dbUser, string(githubTokenJSON), githubUser)
+
+	// create/update the user record
+	userID, err := l.userService.Save(dbUser)
+	if err != nil {
+		logrus.Errorf("saving user to db failed: %s", err.Error())
+		c.Redirect(http.StatusTemporaryRedirect, failRedirectPath)
+		return
+	}
+
+	appToken, err := l.authService.GenerateToken(userID)
+	if err != nil {
+		logrus.Errorf("token generation failed: %s", err.Error())
+		c.Redirect(http.StatusTemporaryRedirect, failRedirectPath)
+		return
+	}
+
+	// for security reasons, avoid using cookies to send the token to client
+	// instead use html with a script that stores the token to localstorage and redirects to homepage
+	// this is the only workaround to send token to client without using cookies
+	// since the client(frontend) can only read headers/response with ajax request, and this call is not ajax
+	c.Header("Content-Type", "text/html")
+	c.String(200, `<!DOCTYPE html><html><body><script>(function(){localStorage.setItem("token","%s");location.replace("/");}());</script></body></html>`, appToken)
+	logrus.Info("github oauth2 callback finished")
+}
+
+func mapUserAttributes(dbUser *user.User, ghToken string, githubUser gh.User) {
+	dbUser.GithubToken = ghToken
+	dbUser.Email = githubUser.GetEmail()
+	dbUser.Name = githubUser.GetName()
+	dbUser.Location = githubUser.GetLocation()
+	dbUser.AvatarURL = githubUser.GetAvatarURL()
+	dbUser.GithubID = githubUser.GetID()
+	dbUser.GithubUsername = githubUser.GetLogin()
+}

backend/internal/httpservice/login_test.go
@@ -1,0 +1,140 @@
+package httpservice
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang/mock/gomock"
+	gh "github.com/google/go-github/v43/github"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+	"github.com/vivekweb2013/gitnoter/internal/auth"
+	"github.com/vivekweb2013/gitnoter/internal/github"
+	"github.com/vivekweb2013/gitnoter/internal/preference"
+	"github.com/vivekweb2013/gitnoter/internal/user"
+	"golang.org/x/oauth2"
+	"gorm.io/gorm"
+)
+
+const (
+	userID          = uint(1012)
+	email           = "john.doe@example.com"
+	name            = "John Doe"
+	location        = "New York"
+	avatarURL       = "http://example.com/avatar"
+	oauth2TokenJSON = `{"access_token":"gho_token","token_type":"bearer","expiry":"0001-01-01T00:00:00Z"}`
+)
+
+const ()
+
+func TestGithubLogin(t *testing.T) {
+	t.Run("should redirect to provider when the github login request is valid", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+		githubService := github.NewMockService(ctrl)
+
+		gin.SetMode(gin.TestMode)
+		router := gin.Default()
+		handler := NewLoginHandler(nil, githubService, nil)
+		githubService.EXPECT().GetAuthCodeURL(gomock.Any()).Return("/")
+
+		router.GET("/api/v1/oauth2/login/github", handler.GithubLogin)
+		response := httptest.NewRecorder()
+		req, _ := http.NewRequest(http.MethodGet, "/api/v1/oauth2/login/github", nil)
+
+		router.ServeHTTP(response, req)
+		assert.Equal(t, http.StatusTemporaryRedirect, response.Code)
+	})
+}
+
+func TestGithubOAuth2Callback(t *testing.T) {
+	t.Run("should save the user(with token) & return token response when callback invoked", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+		authService := auth.NewMockService(ctrl)
+		githubService := github.NewMockService(ctrl)
+		userService := user.NewMockService(ctrl)
+		state := uuid.NewString()
+		authCode := "abcd"
+		appToken := "app_token"
+		var oauthToken oauth2.Token
+		json.Unmarshal([]byte(oauth2TokenJSON), &oauthToken)
+		githubUser := validGithubUser()
+		dbUser := makeDBUser(githubUser, oauth2TokenJSON)
+
+		gin.SetMode(gin.TestMode)
+		router := gin.Default()
+		handler := NewLoginHandler(authService, githubService, userService)
+		githubService.EXPECT().GetToken(gomock.Any(), authCode).Return(oauthToken, nil)
+		githubService.EXPECT().GetUser(gomock.Any(), oauthToken).Return(githubUser, nil)
+		userService.EXPECT().GetByEmail(email).Return(dbUser, nil)
+		userService.EXPECT().Save(dbUser).Return(uint(1), nil)
+		authService.EXPECT().GenerateToken(uint(1)).Return(appToken, nil)
+
+		router.GET("/oauth2/github/callback", handler.GithubOAuth2Callback)
+		response := httptest.NewRecorder()
+		cookie := http.Cookie{
+			Name:     "state",
+			Value:    state,
+			Path:     "/",
+			Expires:  time.Now().Add(10 * time.Minute),
+			HttpOnly: true,
+		}
+		req, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("/oauth2/github/callback?code=%s&state=%s", authCode, state), nil)
+		req.AddCookie(&cookie)
+
+		router.ServeHTTP(response, req)
+		assert.Equal(t, http.StatusOK, response.Code)
+		assert.Contains(t, response.Body.String(), appToken)
+	})
+}
+
+func makeDBUser(githubUser gh.User, token string) user.User {
+	return user.User{
+		Model: gorm.Model{
+			ID:        1,
+			CreatedAt: time.Now(),
+			UpdatedAt: time.Now(),
+		},
+		Email:          *githubUser.Email,
+		Name:           *githubUser.Name,
+		Location:       *githubUser.Location,
+		AvatarURL:      *githubUser.AvatarURL,
+		GithubID:       *githubUser.ID,
+		GithubUsername: *githubUser.Login,
+		GithubToken:    token,
+		DefaultRepo: &preference.DefaultRepo{
+			Model: gorm.Model{
+				ID:        1,
+				CreatedAt: time.Time{},
+				UpdatedAt: time.Time{},
+			},
+			UserID:        1,
+			Name:          repository,
+			Visibility:    visibility,
+			DefaultBranch: branch,
+		},
+	}
+}
+
+func validGithubUser() gh.User {
+	testEmail := email
+	testName := name
+	testLocation := location
+	testAvatarURL := avatarURL
+	githubID := int64(12345)
+	githubUsername := "johndoe"
+	return gh.User{
+		ID:        &githubID,
+		Login:     &githubUsername,
+		Email:     &testEmail,
+		Name:      &testName,
+		Location:  &testLocation,
+		AvatarURL: &testAvatarURL,
+	}
+}