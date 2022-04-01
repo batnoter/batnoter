7d268ad940113a7a982e619d1f6ded669afb3995
Shikhar <ishgupta2015@gmail.com>
2022-04-01 22:15:03 +0530

    Create separate interface for tree node

--- frontend/src/components/Finder.tsx
+++ frontend/src/components/Finder.tsx
@@ -3,39 +3,39 @@ import { Container } from '@mui/material';
 import React, { ReactElement, useEffect } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from '../app/hooks';
-import { deleteNoteAsync, getNotesAsync, Note, selectNotesTree, Tree, TreeUtil } from '../reducer/noteSlice';
+import { deleteNoteAsync, getNotesAsync, selectNotesTree, TreeNode, TreeUtil } from '../reducer/noteSlice';
 import NoteCard from './NoteCard';
 
 
 const Finder = (): ReactElement => {
   const dispatch = useAppDispatch();
-  const tree = useAppSelector(selectNotesTree)
+  const tree = useAppSelector(selectNotesTree);
   const [searchParams] = useSearchParams();
-  let path = decodeURIComponent(searchParams.get('path') || "")
-  path = path === "/" ? "" : path
+  let path = decodeURIComponent(searchParams.get('path') || "");
+  path = path === "/" ? "" : path;
   useEffect(() => {
     dispatch(getNotesAsync(path))
   }, [path, tree])
 
-  const handleDelete = (note: Note) => {
-    dispatch(deleteNoteAsync(note))
+  const handleDelete = (note: TreeNode) => {
+    dispatch(deleteNoteAsync(note));
   }
 
-  const getChildren = (path: string): Tree[] | undefined => {
-    const node = TreeUtil.searchNode(tree, path)
+  const getChildren = (path: string): TreeNode[] | undefined => {
+    const node = TreeUtil.searchNode(tree, path);
     if (node?.cached) {
-      return node.children
+      return node.children;
     }
   }
 
-  const notes = getChildren(path) || [] as Tree[]
+  const notes = getChildren(path) || [] as TreeNode[];
 
   return (
     <Container>
       <Masonry columns={{ xs: 1, md: 3, xl: 4 }} spacing={2}>
         {notes.filter(n => !n.is_dir).map(note => (
           <div key={note.path}>
-            <NoteCard note={note as Note} handleDelete={handleDelete} />
+            <NoteCard note={note} handleDelete={handleDelete} />
           </div>
         ))}
       </Masonry>
--- frontend/src/components/NoteCard.tsx
+++ frontend/src/components/NoteCard.tsx
@@ -1,26 +1,24 @@
 import { DeleteOutlined } from "@mui/icons-material";
 import { Card, CardContent, CardHeader, IconButton, Typography } from "@mui/material";
 import React, { ReactElement } from 'react';
-import { Note } from "../reducer/noteSlice";
+import { TreeNode } from "../reducer/noteSlice";
 
 interface Props {
-  note: Note
-  handleDelete: (note: Note) => void
+  note: TreeNode
+  handleDelete: (note: TreeNode) => void
 }
 
+const MaxCardTextLength = 20
+
 const NoteCard: React.FC<Props> = ({ note, handleDelete }): ReactElement => {
-  const getFirstLine = (str: string) => {
-    return str.split('\n', 1)[0]
-  }
-  return (<div>
+  return (
     <Card elevation={1}>
-      <CardHeader action={<IconButton onClick={() => handleDelete(note)}> <DeleteOutlined /> </IconButton>}
-        title={getFirstLine(note.content)} />
+      <CardHeader action={<IconButton onClick={() => handleDelete(note)}> <DeleteOutlined /> </IconButton>} title={note.name.replace(/(\.md)$/i, '')} />
       <CardContent>
-        <Typography color="textSecondary"> {note.content} </Typography>
+        <Typography color="textSecondary"> {note.content?.substring(0, MaxCardTextLength)} {note.content && note.content.length > MaxCardTextLength && '...'}</Typography>
       </CardContent>
     </Card>
-  </div>)
+  )
 }
 
 export default NoteCard;
