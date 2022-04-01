a120619e7b9101c3f023591847a3146ff438d880
Shikhar <ishgupta2015@gmail.com>
2022-04-02 00:12:01 +0530

    Fix ui tests

--- frontend/src/setupTests.tsx
+++ frontend/src/setupTests.tsx
@@ -3,3 +3,13 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
+import React from "react";
+
+
+jest.mock("react-markdown", () => (props: { children: React.ReactNode }) =>
+  jest.fn(() => <>{props.children}</>),
+);
+
+jest.mock("remark-gfm", () =>
+  jest.fn(() => {/* */ }),
+);
