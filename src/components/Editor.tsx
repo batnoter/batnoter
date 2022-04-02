9c0c8ac61b86ed0ac05e04409277c5f59fc46378
vivekweb2013 <vivekweb2013@gmail.com>
2022-04-02 15:16:37 +0530

    Allow note editing on ui

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,14 +1,15 @@
 
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
-import { Autocomplete, Breadcrumbs, Button, Container, Link, TextField, Typography } from '@mui/material';
+import { Autocomplete, Breadcrumbs, Button, Container, Link, TextField } from '@mui/material';
 import React, { FormEvent, ReactElement, useEffect, useState } from 'react';
 import ReactMarkdown from "react-markdown";
 import MDEditor from 'react-markdown-editor-lite';
 import 'react-markdown-editor-lite/lib/index.css';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
 import remarkGfm from 'remark-gfm';
 import { useAppDispatch, useAppSelector } from '../app/hooks';
-import { saveNoteAsync, selectNotesTree, TreeUtil } from '../reducer/noteSlice';
+import { getNoteAsync, saveNoteAsync, selectNotesTree, TreeUtil } from '../reducer/noteSlice';
+import { getTitleFromFilename } from '../util/util';
 import './Editor.scss';
 
 const Editor: React.FC = (): ReactElement => {
@@ -17,20 +18,44 @@ const Editor: React.FC = (): ReactElement => {
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
   const tree = useAppSelector(selectNotesTree);
-  const [dirPathArray, setDirPathArray] = useState([] as string[]);
-  const [endDir, setEndDir] = useState("");
-  const [dirPathError, setDirPathError] = useState(false);
 
-  const [content, setContent] = useState('');
-  const [contentError, setContentError] = useState(false);
+  const [editMode, setEditMode] = useState(false);
+  const [sha, setSHA] = useState('');
   const [title, setTitle] = useState('');
   const [titleError, setTitleError] = useState(false);
-  const defaultPathOptions = TreeUtil.getChildDirs(tree, "");
-  const [pathOptions, setPathOptions] = useState(defaultPathOptions);
+  const [content, setContent] = useState('');
+  const [contentError, setContentError] = useState(false);
+  const [endDir, setEndDir] = useState('');
+  const [dirPathArray, setDirPathArray] = useState([] as string[]);
+  const [dirPathError, setDirPathError] = useState(false);
+
+  const defaultPathOptions = TreeUtil.getChildDirs(tree, '');
+  const [pathAutoCompleteOptions, setPathAutoCompleteOptions] = useState(defaultPathOptions);
+
+  const { pathname } = useLocation();
+  const { path } = useParams();
 
   useEffect(() => {
-    setPathOptions(defaultPathOptions);
-  }, [tree])
+    if (path != null && pathname.startsWith('/edit/')) {
+      setEditMode(true);
+      const decodedPath = decodeURIComponent(path);
+      const note = TreeUtil.searchNode(tree, decodedPath);
+      if (note == null) {
+        return;
+      }
+      if (!note.cached) {
+        dispatch(getNoteAsync(note.path));
+        return;
+      }
+      const pathArray = path.split('/');
+      pathArray.pop(); // remove the filename from path
+      setDirPathArray(pathArray)
+      setSHA(note?.sha || '');
+      setTitle(getTitleFromFilename(note.name));
+      content == '' && setContent(note?.content || '');
+    }
+    setPathAutoCompleteOptions(defaultPathOptions);
+  }, [tree, path, pathname])
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -58,21 +83,18 @@ const Editor: React.FC = (): ReactElement => {
     }
 
     const fullPath = dirPath !== "" ? (dirPath + '/' + filename) : filename;
-    await dispatch(saveNoteAsync({ path: fullPath, content: content }));
+    await dispatch(saveNoteAsync({ path: fullPath, content: content, sha: sha }));
     navigate("/?path=" + encodeURIComponent(dirPath));
   }
 
   return (
     <Container maxWidth="md">
-      <Typography variant="h6" color="textSecondary" component="h2" gutterBottom >
-        Create a New Note
-      </Typography>
-
       <form noValidate autoComplete="off" onSubmit={handleSubmit}>
-        <Autocomplete freeSolo fullWidth multiple openOnFocus value={dirPathArray} options={pathOptions}
+        <Autocomplete freeSolo fullWidth multiple openOnFocus value={dirPathArray} options={pathAutoCompleteOptions}
+          disabled={editMode}
           onChange={(e, newPath) => {
             setDirPathArray([...newPath]);
-            setPathOptions(TreeUtil.getChildDirs(tree, newPath.join("/")));
+            setPathAutoCompleteOptions(TreeUtil.getChildDirs(tree, newPath.join("/")));
           }}
 
           renderTags={(tagValue) => (
@@ -90,7 +112,7 @@ const Editor: React.FC = (): ReactElement => {
               const newPath = [...dirPathArray, ...trimmedPath.split('/')];
               if (trimmedPath) {
                 setDirPathArray(newPath);
-                setPathOptions(TreeUtil.getChildDirs(tree, newPath.join("/")));
+                setPathAutoCompleteOptions(TreeUtil.getChildDirs(tree, newPath.join("/")));
               }
               setEndDir('');
               return;
@@ -107,14 +129,16 @@ const Editor: React.FC = (): ReactElement => {
 
         <TextField sx={{ my: 2, display: "block" }}
           helperText="Only alphanumeric characters, space and hyphen (-) are allowed."
+          value={title} disabled={editMode}
           onChange={(e) => { setTitleError(false); setTitle(e.target.value) }} label="Note Title"
           variant="outlined" fullWidth required error={titleError}
         />
 
         <MDEditor view={{ menu: true, md: true, html: false }} canView={{ menu: true, md: true, html: true, fullScreen: false, hideMenu: false, both: true }}
+          value={content}
           renderHTML={text => <ReactMarkdown remarkPlugins={[remarkGfm]} >{text}</ReactMarkdown>}
           placeholder="Note Content*" className={"batnoter-md-editor " + (contentError ? "error" : "")}
-          onChange={(h, t) => { setContentError(false); setContent(t?.target.value || '') }} />
+          onChange={({ text }) => { setContentError(false); setContent(text) }} />
 
         <Button type="submit" variant="contained" endIcon={<KeyboardArrowRightIcon />} sx={{ float: 'right' }}> SAVE </Button>
       </form>
--- src/components/Finder.tsx
+++ src/components/Finder.tsx
@@ -1,24 +1,29 @@
 import { Masonry } from '@mui/lab';
 import { Container } from '@mui/material';
 import React, { ReactElement, useEffect } from 'react';
-import { useSearchParams } from 'react-router-dom';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from '../app/hooks';
 import { deleteNoteAsync, getNotesAsync, selectNotesTree, TreeNode, TreeUtil } from '../reducer/noteSlice';
 import NoteCard from './NoteCard';
 
-
 const Finder = (): ReactElement => {
+  const navigate = useNavigate();
   const dispatch = useAppDispatch();
   const tree = useAppSelector(selectNotesTree);
   const [searchParams] = useSearchParams();
   let path = decodeURIComponent(searchParams.get('path') || "");
   path = path === "/" ? "" : path;
+
   useEffect(() => {
     dispatch(getNotesAsync(path))
   }, [path, tree])
 
   const handleDelete = (note: TreeNode) => {
     dispatch(deleteNoteAsync(note));
+  }
+
+  const handleEdit = (note: TreeNode) => {
+    navigate("/edit/" + encodeURIComponent(note.path));
   }
 
   const getChildren = (path: string): TreeNode[] | undefined => {
@@ -35,7 +40,7 @@ const Finder = (): ReactElement => {
       <Masonry columns={{ xs: 1, md: 3, xl: 4 }} spacing={2}>
         {notes.filter(n => !n.is_dir).map(note => (
           <div key={note.path}>
-            <NoteCard note={note} handleDelete={handleDelete} />
+            <NoteCard note={note} handleEdit={handleEdit} handleDelete={handleDelete} />
           </div>
         ))}
       </Masonry>
