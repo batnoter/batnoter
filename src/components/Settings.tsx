f221246e9c29089ef67f4491ed4f789f0624c02d
vivekweb2013 <vivekweb2013@gmail.com>
2022-04-05 11:35:47 +0530

    Fix text alignment on settings view

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -12,12 +12,12 @@ const Settings: React.FC<Props> = ({ user }): ReactElement => {
 
   return (
     <Container maxWidth="sm">
-      <Grid container direction="column">
-        <Grid flexDirection={'column'} justifyContent={'center'} display="flex" >
+      <Grid container direction="column" textAlign="center">
+        <Grid container direction="column">
           <Grid flexGrow={1} sx={{ backgroundImage: `url('${user?.avatar_url}')`, backgroundPosition: "center", filter: "blur(30px)", height: "150px" }}  ></Grid>
           <Avatar alt={user?.name} src={user?.avatar_url} sx={{ width: 100, height: 100, alignSelf: "center", marginTop: "-100px" }} />
         </Grid>
-        <Grid flexDirection={'column'} justifyContent={'center'} display="flex" marginY={2}>
+        <Grid container direction="column" marginY={2}>
           <Typography m={0} variant="h5" gutterBottom component="div"> {user?.name} </Typography>
           <Typography color="textSecondary" variant="body1" gutterBottom component="div"> {user?.location} </Typography>
           {user?.default_repo?.default_branch && <Typography color="textSecondary" m={0} variant="h6" gutterBottom component="div"> Default Repository: {user?.default_repo?.name} (Branch: {user?.default_repo?.default_branch})</Typography>}
