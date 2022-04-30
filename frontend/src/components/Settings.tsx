d083eeeda1951478e8de6a63a200da31c961eddc
Shikhar <ishgupta2015@gmail.com>
2022-04-30 11:07:28 +0530

    Show email if user's name is empty

--- frontend/src/components/Settings.tsx
+++ frontend/src/components/Settings.tsx
@@ -19,10 +19,10 @@ const Settings: React.FC<Props> = ({ user }): ReactElement => {
           <Avatar alt={user?.name} src={user?.avatar_url} sx={{ width: 100, height: 100, alignSelf: "center", marginTop: "-100px" }} />
         </Grid>
         <Grid container direction="column" marginY={2}>
-          <Typography m={0} variant="h5" gutterBottom component="div"> {user?.name} </Typography>
+          <Typography m={0} variant="h5" gutterBottom component="div"> {user?.name || user?.email} </Typography>
           <Typography color="textSecondary" variant="body1" gutterBottom component="div"> {user?.location} </Typography>
           {user?.default_repo?.default_branch && <Typography color="textSecondary" m={0} variant="h6" gutterBottom component="div">Notes Repository: {user?.default_repo?.name} (<SourceBranch sx={{ verticalAlign: 'middle' }} fontSize='inherit' /> {user?.default_repo?.default_branch})</Typography>}
-          <Button onClick={() => setOpenRepoSelectDialog(true)}> Change Notes Repository </Button>
+          <Button onClick={() => setOpenRepoSelectDialog(true)}>Change Notes Repository</Button>
           <RepoSelectDialog open={openRepoSelectDialog} setOpen={setOpenRepoSelectDialog} defaultRepo={user?.default_repo?.name} />
         </Grid>
       </Grid>
