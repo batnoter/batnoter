db470953cb57c2360d6c3357404387ea6ce47a20
vivekweb2013 <vivekweb2013@gmail.com>
2022-06-18 22:02:19 +0530

    Add sponsor link
    Fix #91

--- src/util/util.ts
+++ src/util/util.ts
@@ -7,6 +7,7 @@ export const URL_FAQ = `${URL_REPO}/wiki/FAQ`
 export const URL_ISSUES = `${URL_REPO}/issues`
 export const URL_TWITTER_HANDLE = "https://twitter.com/batnoter";
 export const URL_GITHUB = URL_REPO;
+export const URL_SPONSOR = "https://github.com/sponsors/vivekweb2013";
 
 const REPLACE_EXT_REGEX = /(\.md)$/i;
 const EXT = '.md';
